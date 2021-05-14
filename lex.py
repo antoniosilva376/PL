fe85ffaf963f60d3a46b586bbef4150f784ae347
antoniosilva376 <tugasdonorte@gmail.com>
2021-05-14 16:53:04 +0100

    missing read/write/arrays

--- lex.py
+++ lex.py
@@ -2,16 +2,35 @@ import ply.lex as lex
 import sys
 
 
-literals = ['(',')','+','-','*','/','=','"',';',',','>','<']
 
-tokens = ["Num","Id","Repeat","Int","Read","Write"]
+literals = ['(',')','+','-','*','/','=','"',';',',','>','<','!','{','}']
 
-t_Num = r'\d+'
-t_Repeat = r'repeat-until'
-t_Int = r'int'
-t_Read = r'read'
-t_Write = r'write'
-t_Id  = r'\w+'
+tokens = ["Num","If","Else","Id","Repeat","Int","Read","Write"]
+
+def t_If(t):
+    r'if'
+    return(t)
+def t_Num(t): 
+    r'\d+'
+    return(t)
+def t_Repeat(t): 
+    r'repeat-until'
+    return(t)
+def t_Int(t): 
+    r'int'
+    return(t)
+def t_Read(t): 
+    r'read'
+    return(t)
+def t_Write(t): 
+    r'write'
+    return(t)
+def t_Else(t): 
+    r'else'
+    return(t)
+def t_Id(t):
+    r'\w+'
+    return(t)
 
 t_ignore = " \t\n"
 
@@ -20,6 +39,7 @@ def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()
+
 """
 for line in sys.stdin:
     lexer.input(line)
--- yacc.py
+++ yacc.py
@@ -11,12 +11,8 @@ Instrucoes : Instrucoes Instrucao
 
 Instrucao : Atribuicao
           | Operacao
-          | Condicional
           | Funcao
 
-Funcao : Write '(' Instrucao ')'
-       | Read '(' Instrucao ')'
-       | Repeat '(' Instrucao ')'
 
 Atribuicao : int id '=' Operacao
            | id '=' Operacao
@@ -33,18 +29,31 @@ Fator : Num
       | Id
       | '(' Operacao ')'
 
-ts = {
-    ('a',0,1)
-    ...
-}
+Condicional : Operacao '>' Operacao
+            | Operacao '<' Operacao
+            | Operacao '>' '=' Operacao
+            | Operacao '<' '=' Operacao
+            | Operacao '=' '=' Operacao
+            | Operacao '!' '=' Operacao
 
-
+Funcao : Write '(' ')'
+       | Read '(' ')'
+       | Repeat '(' Condicional ')' '{' Instrucoes '}'
+       | If '(' Condicional ')' '{' Instrucoes '}' Else '{' Instrucoes '}'
+       | If '(' Condicional ')' '{' Instrucoes '}'
 """
 
+#Tabela de Simbolos dict{variavel : pos_stack}
+ts = dict({})
+#variavel que aponta para a posição atual da stack
+pos_stack=0
+#variavel para tornar as etiquetas unicas
+func_nr = 0
 
 def p_LExp(p):
     "LExp : Instrucoes"
     p[0] = p[1]
+
 
 
 def p_Instrucoes_Instrucao(p):
@@ -63,71 +72,147 @@ def p_Instrucao_Operacao(p):
     "Instrucao : Operacao"
     p[0] = p[1]
 
-"""def p_Instrucao_Condicional(p):
-    "Instrucao : Condicional"
-    p[0] = p[1]"""
-
-"""def p_Instrucao_Funcao(p):
+def p_Instrucao_Funcao(p):
     "Instrucao : Funcao"
-    p[0] = p[1]"""
+    p[0] = p[1]
+
+
+
+def p_Funcao_Write(p):
+    "Funcao : Write '(' ')'"
+
+def p_Funcao_Read(p):
+    "Funcao : Read '(' ')'"
+
+def p_Funcao_Repeat(p):
+    "Funcao : Repeat '(' Condicional ')' '{' Instrucoes '}'"
+    global func_nr
+    p[0] = ("\nrepeat" + str(func_nr) +":" 
+        + p[3]+"\nnot\njz end" + str(func_nr)
+        + p[6]
+        + "\njump repeat" + str(func_nr)
+        + "\nend" + str(func_nr) +":"
+        )
+    func_nr+=1
+    
+def p_Funcao_IfElse(p):
+    "Funcao : If '(' Condicional ')' '{' Instrucoes '}' Else '{' Instrucoes '}'"
+    global func_nr
+    p[0] = (p[3]+"\njz else" + str(func_nr)
+        + p[6]
+        +"\njump end" + str(func_nr)
+        +"\nelse" + str(func_nr) + ":"
+        + p[10] 
+        + "\nend" + str(func_nr) +":"
+        )
+    func_nr+=1
+
+def p_Funcao_If(p):
+    "Funcao : If '(' Condicional ')' '{' Instrucoes '}'"
+    global func_nr
+    p[0] = (p[3]+"\njz end" + str(func_nr)
+        + p[6] 
+        + "\nend" + str(func_nr) +":"
+        )
+    func_nr+=1
+
 
 
 def p_Atribuicao_Declaracao(p):
     "Atribuicao : Int Id '=' Operacao"
-    p[0] = p[1] + " " + p[2] + " = " + str(p[4])
-
+    if(p[2] not in ts):
+        global pos_stack
+        ts[p[2]] = pos_stack-1
+        p[0] = str(p[4]) + "\nstoreg " + str(ts[p[2]])
+    else:
+        #erro
+        pass
 
 def p_Atribuicao_Alt(p):
     "Atribuicao : Id '=' Operacao"
-    #verificar se TS[p[1]]
-    #p[0] = "pushi" + p[3] + "\npushg" + TS[p[1]] 
-    p[0] = p[1] + "=" + str(p[3])
+    if(p[1] in ts):
+        p[0] =  str(p[3]) + "\nstoreg " + str(ts[p[1]])
+    else:
+        #erro
+        pass
 
 
 
 def p_Operacao_Mais(p):
     "Operacao : Operacao '+' Termo"
-    p[0] = p[1] + p[3]
+    p[0] = str(p[1]) + str(p[3]) + "\nadd"
+    global pos_stack
+    pos_stack-=1
 
 def p_Operacao_Menos(p):
     "Operacao : Operacao '-' Termo"
-    p[0] = p[1] - p[3]
+    p[0] = str(p[1]) + str(p[3]) + "\nsub"
+    global pos_stack
+    pos_stack-=1
 
 def p_Operacao_Termo(p):
     "Operacao : Termo"
-    p[0] = p[1]
+    p[0] = str(p[1])
 
 
 
-def p_Termo_Mult(p):
+def p_Termo_Mul(p):
     "Termo : Termo '*' Fator"
-    p[0] = p[1] * p[3]
+    p[0] = str(p[1]) + str(p[3]) + "\nmul"
+    global pos_stack
+    pos_stack-=1
 
 def p_Termo_Div(p):
     "Termo : Termo '/' Fator"
-    p[0] = p[1] / p[3]
+    p[0] = str(p[1]) + str(p[3]) + "\ndiv"
+    global pos_stack
+    pos_stack-=1
 
 def p_Termo_Fator(p):
     "Termo : Fator"
-    p[0] = p[1]
-      
+    p[0] = str(p[1])
+
 
 
 def p_Fator_Num(p): 
     "Fator : Num"
-    p[0] = int(p[1])
+    p[0] = "\npushi " + str(p[1])
+    global pos_stack
+    pos_stack+=1
 
 def p_Fator_Id(p):
     "Fator : Id"
-    p[0] = p[1]
-    #P[0] = int(TS[p[1]])
+    p[0] = "\npushg " + str(ts[p[1]])
 
 def p_Fator_Operacao(p):
     "Fator : '(' Operacao ')'"
-    p[0] = p[1]
+    p[0] = p[2]
 
 
 
+def p_Condicional_Maior(p):
+    "Condicional : Operacao '>' Operacao"
+    p[0] = str(p[3]) + str(p[1]) + "\nsup"
+
+def p_Condicional_Menor(p):
+    "Condicional : Operacao '<' Operacao"
+    p[0] = str(p[3]) + str(p[1]) + "\ninf"
+
+def p_Condicional_MaiorIgual(p):
+    "Condicional : Operacao '>' '=' Operacao"
+    p[0] = str(p[4]) + str(p[1]) + "\nsupeq"
+
+def p_Condicional_MenorIgual(p):
+    "Condicional : Operacao '<' '=' Operacao"
+    p[0] = str(p[4]) + str(p[1]) + "\ninfeq"
+
+def p_Condicional_Igual(p):
+    "Condicional : Operacao '=' '=' Operacao"
+    p[0] = str(p[4]) + str(p[1]) + "\nequal"
+
+def p_Condicional_Diferente(p):
+    "Condicional : Operacao '!' '=' Operacao"
+    p[0] = str(p[4]) + str(p[1]) + "\nequal\nnot"
 
 
 
